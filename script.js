62375779873e7df39bb75ccc47f3f21dfc970c28
jparmar5268-ship-it <jparmar5268@gmail.com>
2026-03-27 16:35:05 +0530

    portfolio complete

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-let sections = document.querySelectorAll("section");    // Select all sections (home, about, services, etc.)
-let navLinks = document.querySelectorAll(".navbar a");  // Select all navbar links
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".navbar a");
 
-window.addEventListener("scroll", () => {       // Run this code when user scrolls the page
-    let current = "";
+window.addEventListener("scroll", () => {
+
+    let scrollY = window.scrollY;
 
     sections.forEach(section => {
+
         let sectionTop = section.offsetTop - 150;
         let sectionHeight = section.offsetHeight;
+        let sectionId = section.getAttribute("id");
 
-        if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
-            current = section.getAttribute("id");
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+            });
+
+            document
+                .querySelector('.navbar a[href="#' + sectionId + '"]')
+                .classList.add("active");
         }
+
     });
 
-    navLinks.forEach(link => {  // Remove active class from all navbar links
-        link.classList.remove("active");
-        // Add active class to the link of current section
-        if (link.getAttribute("href") === `#${current}`) {
-            link.classList.add("active");
-        }
+});
+
+
+// prevent all # links from jumping to top
+let buttons = document.querySelectorAll('a[href="#"]');
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault(); // stop default jump
     });
 });
+
+
+
+//========MOBILE========
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle("active");
+};
